de96288a90cf029abd5fbc6a49f792ab238ac50f
Erhan VsCode <erhanselman80-vscode@gmail.com>
2026-06-16 18:19:42 +0300

    guncelle kurs2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import { themes as prismThemes } from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Mobildev API',
-  tagline: 'Api Dökümanları',
+  tagline: 'Mobildev Api Dökümanları',
   favicon: 'img/mobildev-favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
